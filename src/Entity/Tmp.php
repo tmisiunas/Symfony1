412e01308ccb07b0e89af8faf73f57e4c0d20be7
Misiūnas <tmisiunas@gmail.com>
2026-04-16 05:14:43 +0300

    Field Description added to table Tmp

--- src/Entity/Tmp.php
+++ src/Entity/Tmp.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\TmpRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TmpRepository::class)]
@@ -15,6 +16,9 @@ class Tmp
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
 
     public function getId(): ?int
     {
@@ -29,6 +33,18 @@ class Tmp
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
 
         return $this;
     }
